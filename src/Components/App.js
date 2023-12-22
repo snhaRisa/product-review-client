db52c51118c4f6f976b4991197ea7fa3c194923c
Rishav <risasnha1999@gmail.com>
2023-12-22 16:21:37 +0530

    Initial Commit

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+
+const App = ()=>
+{
+    return (
+        <div>
+            <h1>Product Review System !</h1>
+        </div>
+    );
+};
+
+export default App; 
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+
+//important files to import. 
+import App from './Components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+
+root.render(
+    <App/>
+);
